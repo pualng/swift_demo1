2e6d467b127cb98a3ae276ea473aad729ed3c0df
Polo <coding.polo@gmail.com>
2018-01-31 11:37:27 +0800

    新增alert

--- demo2/RecordViewController.swift
+++ demo2/RecordViewController.swift
@@ -9,15 +9,19 @@
 import UIKit
 
 class RecordViewController: UIViewController {
-
+    
     //變數 -（Outlet)
     @IBOutlet weak var segType: UISegmentedControl!
     @IBOutlet weak var txtDate: UITextField!
     @IBOutlet weak var txtMoney: UITextField!
     @IBOutlet weak var txtDesc: UITextView!
+    
+    @IBOutlet weak var btnDel: UIButton!
+    
     //變數
     var source : Record? = nil  // ？  c# nullable
-
+    let dbh : DBHelper = DBHelper.init()  //取得協助方法
+    
     //Lift Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,9 +30,12 @@ class RecordViewController: UIViewController {
             txtMoney.text = "\(source!.amount)"
             txtDesc.text = source!.description
             segType.selectedSegmentIndex = Int(source!.trade_type)
+        }else{
+            //新增模式
+            btnDel.isHidden = true  //隱藏刪除按鈕
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -57,30 +64,32 @@ class RecordViewController: UIViewController {
         //轉換格式
         var df =  DateFormatter()
         df.dateFormat = "yyyy/MM/dd"
-         //逕行轉換
+        //逕行轉換
         let dates:String = df.string(from: sender.date)
-         //設定
+        //設定
         txtDate.text = dates
     }
+    
+
     
     //Action
     @IBAction func btnSave(_ sender: Any) {
         print("save...")
-        //檢查
-        //error --> Alert
+        //檢查變數
+        if(txtDesc.text.isEmpty){
+            alert(Title: "系統提示",Message: "說明不得為空")
+            return
+        }
         
-        
-        
-        //存入資料
-        let dbh : DBHelper = DBHelper.init()  //取得協助方法
-        //判斷是 新增 還是編輯？
+        //寫入資料庫
         if(source == nil){  //新增
+            //整理資料
             let r = Record()
             r.amount = Double(txtMoney.text!)!
             r.trade_date = txtDate.text!
             r.description = txtDesc.text!
             r.trade_type = Int64(segType.selectedSegmentIndex)
-              //dbh.insert(record: r)  //會跳出警告
+              //寫入DB
             _ = dbh.insert(record: r)  //改成 _下底線
         }else{  //編輯
             //更新資料
@@ -88,19 +97,47 @@ class RecordViewController: UIViewController {
             source!.amount = Double(txtMoney.text!)!
             source!.description = txtDesc.text!
             source!.trade_type = Int64(segType.selectedSegmentIndex)
+            //寫入DB
             dbh.update(record: source!) //update
         }
         
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func alert(Title:String, Message:String){
+        let cancelAction = UIAlertAction(title: "確認", style: .default, handler: nil)
+        
+        let alert = UIAlertController(title: Title, message: Message, preferredStyle: .alert)
+            alert.addAction(cancelAction)
+        
+        self.present(alert, animated: true, completion: nil)
     }
-    */
-
+    
+    @IBAction func btnDelClick(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "貼心提醒", message: "刪除後無法復原！請問是否刪除", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("確定刪除", comment: "Default action"), style: UIAlertActionStyle.destructive, handler: { _ in
+            //刪除
+            self.dbh.delete(index: self.source!.id)
+        }))
+        
+        
+        alert.addAction(UIAlertAction(title: NSLocalizedString("取消", comment: "Default action"), style: `.cancel, handler: { _ in
+            //nothing todo
+        }))
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
